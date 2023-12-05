42fddda0118fea80de970f80662a8c4b58226cdf
quevedo2003 <quevedo@uri.edu>
2023-12-04 23:15:17 -0500

    Debugged the front end and back end sides with added errors to see what is going on

--- WebServer/backend/backend_server.cpp
+++ WebServer/backend/backend_server.cpp
@@ -2,6 +2,7 @@
 #include "splay_tree.h"
 
 int main(int argc, char *argv[]) {
+    
     // Create an HTTP server instance using httplib
     httplib::Server backend;
 
@@ -9,18 +10,25 @@ int main(int argc, char *argv[]) {
     STree history;
 
     // Check if command line arguments are provided
-    if (argc == 1) {
+    if (argc == 1){
         // TODO: Add logic for handling command line arguments if needed
     }
 
     // Define an endpoint to handle GET requests for retrieving the tree data
     backend.Get("/get_tree", [&](const httplib::Request& /*req*/, httplib::Response &res) {
-        // Respond with the JSON representation of the splay tree
-        res.set_content(history.toJson(), "application/json");
+        try {
+            // Respond with the JSON representation of the splay tree
+            res.set_content(history.toJson(), "application/json");
+        } catch (const std::exception& e) {
+            // Handle exceptions and respond with an error
+            res.status = 500;
+            res.set_content("{\"error\": \"" + std::string(e.what()) + "\"}", "application/json");
+        }
     });
 
-    // Uncomment the following block if you want to implement a search_tree endpoint
-    /*
+
+
+    // Define the /search_tree endpoint
     backend.Get("/search_tree", [&](const httplib::Request& req, httplib::Response &res) {
         // Extract the search query from the request parameters
         std::string query = req.get_param_value("query");
@@ -39,7 +47,7 @@ int main(int argc, char *argv[]) {
         // Respond with the updated JSON representation of the splay tree
         res.set_content(history.toJson(), "application/json");
     });
-    */
+
 
     // Start the backend server and listen on localhost:8080
     backend.listen("localhost", 8080);
